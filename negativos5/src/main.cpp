01f43bf929b0f2704b1f1bafaa08cb3d2c138bf7
Deyvid William <deyvidwms@gmail.com>
2022-03-31 18:16:05 -0300

    Resolução questão negativos5

--- negativos5/src/main.cpp
+++ negativos5/src/main.cpp
@@ -7,6 +7,20 @@ const int SIZE = 5; // input size.
 int main(void)
 {
     // TODO: Adicione aqui seu código.
+    int cont = 0;
+
+    int array[SIZE] = {0};
+
+    for (int i = 0; i < SIZE; i++) {
+        cin>>array[i];
+    }
+
+    for (int j = 0; j < SIZE; j++) {
+        if (array[j]<0)
+            cont++;
+    }
+
+    cout<<cont<<"\n";
 
     return 0;
 }
